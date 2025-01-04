b74524dd92f7c44a4185914b4ec7606bcf361483
MahmoudAdelKamal <mahmoudadel2310@gmail.com>
2025-01-04 17:02:42 +0200

    feat: add validation on the getAllProducts dto

--- src/product/dto/get-all-products.dto.ts
+++ src/product/dto/get-all-products.dto.ts
@@ -1,3 +1,31 @@
+import {
+  IsArray,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ArrayMinSize,
+} from "class-validator";
+
 export class GetAllProductsDTO {
+  @IsArray()
+  @IsString({ each: true })
+  @ArrayMinSize(1)
+  @IsOptional()
   categories?: string[];
+
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  area?: string;
+
+  @IsNumber()
+  @IsOptional()
+  page?: number;
+
+  @IsNumber()
+  @IsOptional()
+  limit?: number;
 }
